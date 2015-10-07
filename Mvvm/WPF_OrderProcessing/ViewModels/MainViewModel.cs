a3ee4d6b8b0f4ca065d4970e972b241475d800a0
Thibaut David <thibautdavidpro@outlook.com>
2015-10-07 17:05:50 +0200

    add Mvvm projects

---
+++ Mvvm/WPF_OrderProcessing/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace WPF_OrderProcessing.ViewModels
+{
+    public class MainViewModel : ViewModel
+    {
+        private ViewModel _current;
+
+        public ViewModel Current
+        {
+            get { return _current; }
+            set { _current = value; OnPropertyChanged();}
+        }
+
+        public MainViewModel()
+        {
+            _current = new MessageViewModel {Message = "Click on command"};
+        }
+    }
+}
---
+++ Mvvm/WPF_OrderProcessing/ViewModels/MessageViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WPF_OrderProcessing.ViewModels
+{
+    public class MessageViewModel : ViewModel
+    {
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value; OnPropertyChanged();}
+        }
+    }
+}
